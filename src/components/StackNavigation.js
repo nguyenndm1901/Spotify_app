62261d538d83f06d6fa4440c70c314bb4daa637e
Nguyễn Đức Minh Nguyên <43044839+nguyenndm1901@users.noreply.github.com>
2020-10-26 22:49:05 +0700

    Update StackNavigation.js

--- src/components/StackNavigation.js
+++ src/components/StackNavigation.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 import Settings from "../screens/Settings";
 import SearchScreen from "../screens/SearchScreen";
+import LibraryScreen from "../screens/LibraryScreen"
 
 const Stack = createStackNavigator();
 
@@ -20,6 +21,14 @@ const SearchStackNavigation = () => {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Search" component={SearchScreen} />
+        </Stack.Navigator>
+    );
+};
+
+const LibraryStacknavigation = () => {
+    return (
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Library" component={LibraryScreen} />
         </Stack.Navigator>
     );
 };
